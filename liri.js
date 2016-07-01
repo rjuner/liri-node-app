bfb56473a4a16221215253b754820ef296939499
Rhea Rivera <rjuner@gmail.com>
2016-07-01 13:39:43 -0500

    installed NPMs

--- liri.js
+++ liri.js
@@ -1,5 +1,10 @@
+var fs = require('fs');
+
 var keys = require('./keys.js');
 var Twitter = require('twitter'); 
 var spotify = require('spotify');
 var request = require('request'); 
 
+var nodeArgs = process.argv;
+
+console.log(nodeArgs);

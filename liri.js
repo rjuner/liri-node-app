227817021b5ffeb588bf3281c259404976a13b91
Rhea Rivera <rjuner@gmail.com>
2016-07-01 16:56:21 -0500

    twitter npm loop working...mostly?

--- liri.js
+++ liri.js
@@ -1,10 +1,25 @@
 var fs = require('fs');
 
-var keys = require('./keys.js');
 var Twitter = require('twitter'); 
 var spotify = require('spotify');
 var request = require('request'); 
 
+var keys = require('./keys.js');
+
 var nodeArgs = process.argv;
 
 console.log(nodeArgs);
+
+var client = new Twitter(keys.twitterKeys);
+
+var params = {screen_name: 'rjuner', count: 30, include_rts: false};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+  	for(i = 0; i < 20; i++){
+    console.log(tweets[i].text);
+	}
+  }
+});
+
+//data.tracks.items[0]
